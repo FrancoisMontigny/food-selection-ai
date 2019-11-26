2d5ba136fc14ed1821d5a066688d8ebadfebf610
FrancoisMontigny <francois.montigny@gmail.com>
2019-11-26 13:59:49 -0500

    feat: process column headers before the rest of the file

--- food-selection-ai/src/DataMigrator/Migrator.java
+++ food-selection-ai/src/DataMigrator/Migrator.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Migrator {
+    private static final int EXPECTED_COLUMNS = 47;
+
     private CSVReader reader;
 
     public Migrator(CSVReader reader) {
@@ -21,6 +23,18 @@ public class Migrator {
         AlimentFactory af = new AlimentFactory();
 
         Scanner scanner = new Scanner(new File(csvPath));
+
+        if (!scanner.hasNext()) { // Empty file
+            return null;
+        }
+
+        // The first line is the columns' headers
+        List<String> headers = reader.parseLine(scanner.nextLine(), ' ', ' ');
+
+        if (headers.size() != EXPECTED_COLUMNS) {
+            return null;
+        }
+
         while (scanner.hasNext()) {
             List<String> line = reader.parseLine(scanner.nextLine(), ' ', ' ');
             System.out.println(line.get(0));
