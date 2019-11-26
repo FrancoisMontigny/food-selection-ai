40fa281120c550f57ab28a63703e264a0386f754
FrancoisMontigny <francois.montigny@gmail.com>
2019-11-26 14:12:05 -0500

    feat: added indexes service to read food tables

---
+++ food-selection-ai/src/DataMigrator/IndexesService.java
@@ -0,0 +1,53 @@
+package DataMigrator;
+
+/**
+ * This class provides indexes to properly read data from a food table.
+ */
+public class IndexesService {
+    private static final int DESCRIPTION = 0;
+    private static final int ENERGY = 1;
+    private static final int PROTEIN = 2;
+    private static final int CARBOHYDRATE = 3;
+    private static final int SUGARS_TOTAL = 4;
+    private static final int FIBER_TOTAL_DIETARY = 5;
+    private static final int FAT_TOTAL = 6;
+    private static final int FATTY_ACIDS_TOTAL_SATURATED = 7;
+    private static final int FATTY_ACIDS_TOTAL_MONOUNSATURATED = 8;
+    private static final int FATTY_ACIDS_TOTAL_POLYUNSATURATED = 9;
+    private static final int CHOLESTEROL = 10;
+    private static final int RETINOL = 11;
+    private static final int A_VITAMIN_MCE = 12;
+    private static final int CAROTENE_ALPHA = 13;
+    private static final int CAROTENE_BETA = 14;
+    private static final int CRYPTOXANTHIN_BETA = 15;
+    private static final int LYCOPENE = 16;
+    private static final int LUTEIN_AND_ZEAXANTHIN = 17;
+    private static final int THIAMIN = 18;
+    private static final int RIBOFLAVIN = 19;
+    private static final int NIACIN = 20;
+    private static final int B_6_VITAMIN = 21;
+    private static final int FOLIC_ACID = 22;
+    private static final int FOLATE_FOOD = 23;
+    private static final int FOLATE_DFE = 24;
+    private static final int FOLATE_TOTAL = 25;
+    private static final int CHOLINE_TOTAL = 26;
+    private static final int B_12_VITAMIN = 27;
+    private static final int B_12_VITAMIN_ADDED = 28;
+    private static final int C_VITAMIN = 29;
+    private static final int D2_D3_VITAMIN = 30;
+    private static final int E_VITAMIN_ALPHA_TOCOPHEROL = 31;
+    private static final int E_VITAMIN_ADDED = 32;
+    private static final int K_VITAMIN_PHYLLOQUINONE = 33;
+    private static final int CALCIUM = 34;
+    private static final int PHOSPHORUS = 35;
+    private static final int MAGNESIUM = 36;
+    private static final int IRON = 37;
+    private static final int ZINC = 38;
+    private static final int COPPER = 39;
+    private static final int SELENIUM = 40;
+    private static final int POTASSIUM = 41;
+    private static final int SODIUM = 42;
+    private static final int CAFFEINE = 43;
+    private static final int THEOBROMINE = 44;
+    private static final int ALCOHOL = 45;
+    private static final int FOOD_GROUP = 46;}
